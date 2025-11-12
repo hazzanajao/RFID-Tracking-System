a06dd922427ae1a32616829aed6d1b5961c610d3
Hazzan Aj <hazzanaj@gmail.com>
2025-11-12 13:10:07 +0200

    Implemented the Resource not found exception

---
+++ src/main/java/com/rfidDevExpertHub/rfidTrackingSystems/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.rfidDevExpertHub.rfidTrackingSystems.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
